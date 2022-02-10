64696ea315aafbc0472d61bc59dbfafe691cab74
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2022-02-10 19:06:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const nodemailer = require('nodemailer');
 const {google} = require('googleapis');
+const config = require('./config.js');
+
 const OAuth2 = google.auth.oauth2;
 
+const OAuth2_client = new OAuth2(config.clientId, config.clientSecret)
+OAuth2_client.setCredentials({refresh_token : config.refresh_token})
 
-con
+function sendMail(){
+    const  accessToken = OAuth2_client.getAccessToken()
+
+    const transport = nodemailer.createTransport({
+        service:"gmail"
+        auth:{
+            
+        }
+    })
+
+
+}
